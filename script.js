e3ec3ae7d92d2a9a43d5c01c46d7aeba47799f82
kevinalappat09 <kevin.d.alappat@gmail.com>
2023-10-01 11:57:53 +0530

    Added UI to the game

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const rock_btn = document.querySelector('#rock');
+const paper_btn = document.querySelector('#paper');
+const scissors_btn = document.querySelector('#scissors');
+
+const play_rnd_btn = document.querySelector('#playrnd');
+
+const score_count_element = document.querySelector('.score-count');
+
+const user_message_element = document.querySelector('.user-msg');
+const choice_message_element = document.querySelector('.choice-msg');
+
+let score_count = 0;
+let selected_user_input;
+
 function get_computer_choice() {
     //Returns a random value between 1 and 3
     let rand_choice = Math.floor((Math.random() * 3)+1);
@@ -18,55 +32,61 @@ function get_computer_choice() {
 function play_round(user_choice, computer_choice) {
     //Ensures that user string is case insensitive
     user_choice = user_choice.toLowerCase();
-    console.log(`You chouse ${user_choice} & the computer chose ${computer_choice}`);
+    choice_message_element.textContent = `You chose ${user_choice} & the computer chose ${computer_choice}`;
     if(user_choice==computer_choice) {
-        console.log("It's a tie");
-        return 0;
+        user_message_element.textContent = `It's a tie"`;
     } else if(user_choice=="rock" && computer_choice=="paper") {
-        console.log(`Paper beats rock, Computer Wins!`);
-        return -1;
+        user_message_element.textContent = `Paper bears rock, computer wins`;
+        score_count--;
     } else if(user_choice=="rock" && computer_choice=="scissors") {
-        console.log(`Rock beats scissors, User Wins!`);
-        return 1;
+        user_message_element.textContent = `Rock beats scissors, user wins`;
+        score_count++;
     } else if(user_choice=="paper" && computer_choice=="rock") {
-        console.log(`Paper beats rock, User Wins!`);
-        return 1;
+        user_message_element.textContent = `Paper beats rock, user wins`;
+        score_count++;
     } else if(user_choice=="paper" && computer_choice=="scissors") {
-        console.log(`Scissor beats paper, Computer Wins!`);
-        return -1;
+        user_message_element.textContent = `Scissors beat paper, computer wins`;
+        score_count--;
     } else if(user_choice=="scissors" && computer_choice=="rock") {
-        console.log(`Rock beats scissors, Computer Wins!`);
-        return -1;
+        user_message_element.textContent = `Rock beats scissors, computer wins`;
+        score_count--;
     } else if(user_choice=="scissors" && computer_choice=="paper") {
-        console.log(`Scissors beats paper. User Wins!`);
-        return 1;
+        user_message_element.textContent = `Scissors beats paper, user wins`;
+        score_count++;
+    }
+    else {
+        user_message_element.textContent = `Select one of the options first.`;
     }
 }
 
-function get_user_input() {
-    let user_input = prompt("Enter your choice for this round ");
-    return user_input;
-}
+rock_btn.addEventListener('click', () => {
+    selected_user_input = "Rock";
+    choice_message_element.textContent = ``;
+    user_message_element.textContent = `Rock was selected`;
+}) 
 
-function game() {
-    let score_count = 0;
-    user_input = get_user_input();
-    score_count += play_round(user_input,get_computer_choice());
+scissors_btn.addEventListener('click',() => {
+    selected_user_input = 'Scissors';
+    choice_message_element.textContent = ``;
+    user_message_element.textContent = `Scissors was selected`;
+})
 
-    user_input = get_user_input();
-    score_count += play_round(user_input,get_computer_choice());
+paper_btn.addEventListener('click', () => {
+    selected_user_input = 'Paper';
+    choice_message_element.textContent = ``;
+    user_message_element.textContent = `Paper was selected`;
+})
 
-    user_input = get_user_input();
-    score_count += play_round(user_input,get_computer_choice());
 
-    user_input = get_user_input();
-    score_count += play_round(user_input,get_computer_choice());
+play_rnd_btn.addEventListener('click', () => {
+    play_round(selected_user_input,get_computer_choice());
+    console.log(score_count);
+    score_count_element.textContent = `Score : ${score_count}`;
 
-    user_input = get_user_input();
-    score_count += play_round(user_input,get_computer_choice());
+    if(score_count == 5) {
+        user_message_element.textContent = `User wins`;
+    } else if (score_count == -5) {
+        user_message_element.textContent = `Computer Wins`;
+    }
+});
 
-    return score_count;
-}
-
-let final_score = game();
-console.log(final_score);
